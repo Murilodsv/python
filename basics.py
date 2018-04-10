15596fd0270ce14b74de063bc90388c582b3256d
Murilodsv <murilodsv@usp.br>
2018-04-10 11:07:49 -0300

    list creation

--- basics.py
+++ basics.py
@@ -1,2 +1,16 @@
 #--- Basic files to get intuition about python sytaxis
 
+#--- Set lists
+l = range(10);
+l = [1,50]
+
+#--- In order to print or "end" your loop you need to press "enter" when in console mode 
+l = []
+for i in range(9):
+    l.append(i)
+    print(l)
+    
+#--- Alternatively you can run it within exec()
+l = []
+exec("for i in range(9): l.append(i)")
+print(l)
